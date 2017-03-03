6487488f873e90d2f8ec60168fef454396ab97f9
Galina Yakovenko <gyakovenko@gmail.com>
2017-03-02 16:56:23 -0800

    Update TrackingPage.java

--- src/main/java/narvar/TrackingPage.java
+++ src/main/java/narvar/TrackingPage.java
@@ -1,5 +1,5 @@
 /**
- *   File Name: trackingPage.java<br>
+ *   File Name: TrackingPage.java<br>
  *
  *   Yakovenko, Galina<br>
  *   Created: Mar 2, 2017
@@ -12,58 +12,92 @@ import org.apache.log4j.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.*;
 
-import com.sqa.gy.helpers.*;
-
 /**
- * trackingPage Locates objects in first tracking section Est. Date of Delivery:
+ * TrackingPage Locates objects in first tracking section Est. Date of Delivery:
  * date, month, panel-title(status) Locates objects in second tracking section
  * Actions: status, date of last action, last action description Initializes
  * logger
- * 
+ *
  * @author Yakovenko, Galina
  *
  */
-public class trackingPage {
+public class TrackingPage {
+	
+	@FindBy(xpath = "//div[@class='edd-date']")
+	private static WebElement eddDate;
+
+	@FindBy(xpath = "//div[@class='title panel-title']")
+	private static WebElement eddDeliveryStatus;
+
+	@FindBy(xpath = "//div[@class='edd-month']")
+	private static WebElement eddMonth;
+
+	@FindBy(xpath = "//div[@class='description-container’]/span[@class='description’]")
+	private static WebElement statusActionDescription;
+
+	@FindBy(xpath = "//div[@class=‘tracking-status-container’]/h2")
+	private static WebElement statusHeading;
+
+	@FindBy(xpath = "//div[@class='timestamp’]")
+	private static WebElement statusTimestamp;
+
+	public static boolean checkActivityDesc() {
+		String statusFullText = statusHeading.getText();
+		return seeIfEqualsDelivered(statusFullText);
+	}
+
+	public static boolean checkActivityStatus() {
+		String statusFullText = statusHeading.getText();
+		return seeIfEqualsDelivered(statusFullText);
+	}
+
+	public static boolean checkEddStatus() {
+		String statusFullText = eddDeliveryStatus.getText();
+		return seeIfEqualsDelivered(statusFullText);
+	}
+
+	public static String retrieveAbbEddMo() {
+		String monthFullText = eddMonth.getText();
+		String month = monthFullText.trim();
+		String monthAbb = month.substring(0, 3);
+		return monthAbb;
+	}
+
+	public static String[] retrieveActivityDate() {
+		String dateFullText = statusTimestamp.getText();
+		String date = dateFullText.trim();
+		String[] dateInPieces = date.split(" ");
+		return dateInPieces;
+	}
+
+	public static String retrieveEddDate() {
+		String dateFullText = eddDate.getText();
+		String date = dateFullText.trim();
+		return date;
+	}
+
+	private static boolean seeIfEqualsDelivered(String statusFullText) {
+		String status = statusFullText.trim();
+		if (status.equalsIgnoreCase("delivered")) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
 	public Logger logger;
 
 	private WebDriver driver;
 
-	@FindBy(xpath = "//div[@class='edd-date']/text()")
-	private WebElement eddDate;
-
-	@FindBy(xpath = "//div[@class='title panel-title']/text()")
-	private WebElement eddDeliveryStatus;
-
-	@FindBy(xpath = "//div[@class='edd-month']/text()")
-	private WebElement eddMonth;
-
-	@FindBy(xpath = "//div[@class='description-container’]/span[@class='description’]/text()")
-	private WebElement statusActionDescription;
-
-	@FindBy(xpath = "//div[@class=‘tracking-status-container’]/h2/text()")
-	private WebElement statusHeading;
-
-	@FindBy(xpath = "//div[@class='timestamp’]/text()")
-	private WebElement statusTimestamp;
-
 	/**
-	 * Constructs trackingPage instance. Uses BasicPage constructor
+	 * Constructs TrackingPage instance. Uses BasicPage constructor
 	 *
 	 * @param driver
 	 */
-	public trackingPage(WebDriver driver) {
+	public TrackingPage(WebDriver driver) {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
-		this.logger = Logger.getLogger(BasicPage.class);
-	}
-
-	public Logger getLogger() {
-		return this.logger;
-	}
-
-	private void setLogger(Logger logger) {
-		this.logger = logger;
+		this.logger = Logger.getLogger(TrackingPage.class);
 	}
 
 }
