5e897c0924f3d156a7469e2106e84b4384547e59
Galina Yakovenko <gyakovenko@gmail.com>
2017-03-03 09:19:42 -0800

    Create BasticTest.java and TrackingPageTest.java

---
+++ src/test/java/narvar/BasicTest.java
@@ -0,0 +1,84 @@
+/**
+ *   File Name: BasicTest.java<br>
+ *
+ *   Yakovenko, Galina<br>
+ *   Created: Mar 2, 2017
+ *
+ */
+
+package narvar;
+
+import java.util.concurrent.*;
+
+import org.apache.log4j.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+import org.openqa.selenium.firefox.*;
+import org.testng.annotations.*;
+
+/**
+ * BasicTest to be extended by narvar TrackingPageTest Sets up driver with
+ * baseURL before every test Tears down after every test
+ *
+ * @author Yakovenko, Galina
+ */
+public class BasicTest {
+
+	private String baseUrl;
+	private WebDriver driver;
+	private Logger logger;
+
+	/**
+	 * Constructs instance of BasicTest and initiates logger
+	 *
+	 * @param baseUrl
+	 */
+	public BasicTest(String baseUrl) {
+		this.baseUrl = baseUrl;
+		this.logger = Logger.getLogger(BasicTest.class);
+	}
+
+	public WebDriver getDriver() {
+		return this.driver;
+	}
+
+	public Logger getLogger() {
+		return this.logger;
+	}
+
+	/**
+	 * sets up Chrome Driver before each test - when enabled
+	 */
+	@BeforeMethod // (groups = "chrome")
+	(enabled = false)
+	public void setUpChrome() {
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		driver.get(getBaseUrl());
+	}
+
+	/**
+	 * sets up Firefox Driver before each test - when enabled
+	 */
+	@BeforeMethod // (groups = "firefox")
+	// (enabled = false)
+	public void setUpFirefox() {
+		this.driver = new FirefoxDriver();
+		this.driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		this.driver.get(getBaseUrl());
+	}
+
+	/**
+	 * tears down and closes driver after each test
+	 */
+	@AfterMethod
+	public void tearDown() {
+		getDriver().quit();
+	}
+
+	private String getBaseUrl() {
+		return this.baseUrl;
+	}
+
+}
---
+++ src/test/java/narvar/TrackingPageTest.java
@@ -0,0 +1,93 @@
+/**
+ *   File Name: TrackingPageTest.java<br>
+ *
+ *   Yakovenko, Galina<br>
+ *   Created: Mar 2, 2017
+ *
+ */
+
+package narvar;
+
+import static org.testng.Assert.*;
+
+import org.testng.annotations.*;
+
+/**
+ * TrackingPageTest Tests page to verify that the Est. Delivery Date section and
+ * the Actions and Status section display the same delivery status and delivery
+ * date
+ *
+ * @author Yakovenko, Galina
+ */
+public class TrackingPageTest extends BasicTest {
+
+	private static String baseURL = "http://ship.sephora.com/tracking/sephora/ontrac?dzip=94002&country=US&lang=EN&tracking_numbers=C11504830885106";
+
+	public TrackingPageTest() {
+		super(baseURL);
+	}
+
+	@Test
+	public void testDeliveryStatusAndDatesMatch() {
+		TrackingPage trackingPage = new TrackingPage(getDriver());
+		// find values for all elements to be compared
+		getLogger().info("Getting values from site: ");
+		boolean eddStatusDelivered = trackingPage.checkEddStatus();
+		getLogger().info("eddStatusDelivered = " + eddStatusDelivered);
+
+		String eddMonthAbb = trackingPage.retrieveAbbEddMo();
+		getLogger().info("eddMonthAbb = " + eddMonthAbb);
+
+		String eddDate = trackingPage.retrieveEddDate();
+		getLogger().info("eddDate = " + eddDate);
+
+		boolean activityStatusDelivered = trackingPage.checkActivityStatus();
+		getLogger().info("activityStatusDelivered = " + activityStatusDelivered);
+
+		String[] activityDate = trackingPage.retrieveActivityDate();
+		getLogger().info("activityDate = " + activityDate[0] + " " + activityDate[1]);
+
+		boolean activityDescIsDelivered = trackingPage.checkActivityDesc();
+		getLogger().info("activityDescIsDelivered = " + activityDescIsDelivered);
+
+		// compare the values
+		getLogger().info("\nNow comparing the values: ");
+		boolean[] matches = compareStatusAndDates(eddStatusDelivered, activityStatusDelivered, activityDescIsDelivered,
+				eddMonthAbb, activityDate, eddDate);
+
+		// Assert: if not yet delivered, just status. If delivered then also
+		// date
+		if (eddStatusDelivered != true) {
+			assertEquals(matches[0], true);
+		} else {
+			boolean[] expected = { true, true };
+			assertEquals(matches, expected);
+		}
+	}
+
+	private boolean[] compareStatusAndDates(boolean eddStatusDelivered, boolean activityStatusDelivered,
+			boolean activityDescIsDelivered, String eddMonthAbb, String[] activityDate, String eddDate) {
+		boolean[] matches = { false, false };
+		if (eddStatusDelivered == activityStatusDelivered && activityStatusDelivered == activityDescIsDelivered) {
+			matches[0] = true;
+			getLogger().info("Delivery status matches for all three fields");
+			if (eddStatusDelivered == true) {
+				getLogger().info("eddMonthAbb  = \"" + eddMonthAbb + "\"");
+				getLogger().info("activityDate[0]  = \"" + activityDate[0] + "\"");
+				getLogger().info("eddDate  = \"" + eddDate + "\"");
+				getLogger().info("activityDate[1]  = \"" + activityDate[1] + "\"");
+
+				if (eddMonthAbb.equalsIgnoreCase(activityDate[0]) && eddDate.equalsIgnoreCase(activityDate[1])) {
+					matches[1] = true;
+					getLogger().info("Delivery date matches for 2 sections");
+				} else {
+					getLogger().info("Delivery date does not match for 2 sections");
+				}
+			}
+		} else {
+			getLogger().info("Delivery status does not match for all three fields");
+		}
+		return matches;
+	}
+
+}
